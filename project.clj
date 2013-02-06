f26e0827a6cad2db306dcb135f10277b1bc2789a
Kevin Downey <redchin@gmail.com>
2013-02-05 18:44:12 -0800

    0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.thelastcitadel/bytes "0.1.0-SNAPSHOT"
+(defproject com.thelastcitadel/bytes "0.1.0"
   :description "data reader for base64 encoded bytes"
-  :url ""
+  :url "https://github.com/hiredman/bytes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
